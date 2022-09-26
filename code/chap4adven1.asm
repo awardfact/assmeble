318d0c05e8a4ee1de463fbb0e1de348ece2d46c8
unknown <awardfact@hanmail.net>
2022-09-26 19:01:37 +0900

    '0926'

--- code/chap4adven1.asm
+++ code/chap4adven1.asm
@@ -9,11 +9,24 @@ CMAIN:
     PRINT_DEC 2, input_data
     NEWLINE
     
-    mov ecx , 8
-
-    L1:
-    mov ax, input_data   
     
+    ;16번 반복하기 위해 cx에 16을 넣어준다
+    mov cx , 16
+    ;bx에는 선언한 변수를 넣어준다 
+    mov bx, [input_data]
+
+    ;ax에 bx값을 넣고 ax에 SHR15를 해주면 가장 왼쪽 비트가 출력되는데 bx를 왼쪽으로 1비트씩 이동시키면서 반복하면 2진수값이 출력된다
+    L1:
+    mov ax, bx
+
+    SHR ax, 15
+    PRINT_DEC 2, ax
+
+    SHL bx, 1
+
+    dec cx
+    CMP cx, 0
+    JG L1
     
     ;write your code here
     xor rax, rax
